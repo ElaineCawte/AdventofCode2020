1d17c87efd288ce4ab35a58e0bd676a3a81d6c58
B1tchDuckD01an <elaine.g.dore@gmail.com>
2020-12-17 07:40:30 +0000

    Day 8

--- Day8/day8.kts
+++ Day8/day8.kts
@@ -1,49 +1,70 @@
 import java.io.File
+import java.lang.Exception
 
 val instructions = File("input").readLines()
 
+//parse ops
 fun getops(i : List<String>):List<Pair<String,Int>>{
     val thestuff = i.map { Pair(it.split(" ")[0],it.split(" ")[1].toInt())}
     return thestuff
 }
 
-fun ac(inlist : List<Pair<String,Int>>)
+//run the code
+fun ac(inlist : MutableList<Pair<String,Int>>)
 {
     var accu = 0
     val hitlist = mutableListOf<Int>()
-
     //process the instruction
-    fun process(ins: Pair<String,Int>, i : Int)
-    {
-        if(hitlist.contains(i))
-        {
-            println("gottem all")
+    fun process(thelist :MutableList<Pair<String,Int>>, ins: Pair<String,Int>, i : Int){
+        // find the instructions which go to the end
+        if (i >= thelist.lastIndex){
+            println("The accumulator contains ")
             println(accu)
             return
-        }
-        if(ins.first == "acc")
-        {
-            accu = accu + ins.second
-            hitlist.add(i)
-            process(inlist[i+1],i+1)
-        }
-        if(ins.first == "jmp")
-        {
-            hitlist.add(i)
-            process(inlist[i+ins.second],i+ins.second)
-        }
-        if(ins.first == "nop")
-        {
-            hitlist.add(i)
-            process(inlist[i+1],i+1)
+            }
+            else
+            {
+                //only do it one loop
+                if(hitlist.contains(i))
+                    return
+                if (ins.first == "acc") {
+                    accu = accu + ins.second
+                    hitlist.add(i)
+                    process(thelist, thelist[i + 1], i + 1)
+                }
+                if (ins.first == "jmp") {
+                    hitlist.add(i)
+                    process(thelist, thelist[i + ins.second], i + ins.second)
+                }
+                if (ins.first == "nop") {
+                    hitlist.add(i)
+                    process(thelist, thelist[i + 1], i + 1)
+                }
+            }
+    }
+
+    fun fixtheList(firstList : MutableList<Pair<String,Int>>, operationToChange : String , operationToChangeTo : String)
+    {
+        firstList.forEachIndexed() {
+            index, pair ->
+            val newlist = firstList.toMutableList()
+            if(pair.first.equals(operationToChange)) {
+                accu = 0
+                newlist[index] = Pair(operationToChangeTo,firstList[index].second)
+                hitlist.clear()
+                process(newlist, newlist[0], 0)
+            }
         }
     }
-    process(inlist[0],0)
+
+    fixtheList(inlist,"jmp","nop")
 }
+
+
 
 fun main(){
     val ops = getops(instructions)
-    ac(ops)
+    ac(ops.toMutableList())
 }
 
 main()
