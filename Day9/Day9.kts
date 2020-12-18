6a70501a876e4fc2a87bfaaab893378838853e62
B1tchDuckD01an <elaine.g.dore@gmail.com>
2020-12-18 12:21:14 +0000

    Day 9 part 2

--- Day9/Day9.kts
+++ Day9/Day9.kts
@@ -3,14 +3,15 @@ import java.io.File
 
 val numbers = File("input").readLines().map { it.toLong() }
 
-//split first 25 numbers from the list
-fun getpreambles(li : List<Long>): MutableList<Pair<List<Long>,Long>> =
-        li.windowed(26).fold(mutableListOf<Pair<List<Long>,Long>>(), {
+//get preamble using window of specified size
+fun getpreambles(li : List<Long>,windowsize : Int): MutableList<Pair<List<Long>,Long>> =
+        li.windowed(windowsize).fold(mutableListOf<Pair<List<Long>,Long>>(), {
         acc, list ->
-        acc.add(Pair(list.slice(0..24),list[25]))
+        acc.add(Pair(list.slice(0..windowsize-2),list[windowsize-1]))
         acc
     })
 
+//get list of preamble numbers into permissable pairs w/ no duplicates
 fun preamblePairs(preamble : List<Long>):MutableList<Pair<Long,Long>> =
         preamble.fold(mutableListOf<Pair<Long,Long>>(), { acc, _ ->
         for (i in 0 until preamble.size)
@@ -21,6 +22,7 @@ fun preamblePairs(preamble : List<Long>):MutableList<Pair<Long,Long>> =
         acc
     })
 
+// true when numbers add up to tested number
 fun aresumof(test : Long, preamble : MutableList<Pair<Long,Long>>):Boolean = (true in preamble.fold(mutableListOf<Boolean>(), { acc, a ->
             if (a.first + a.second == test) {
                 acc.add(true)
@@ -30,35 +32,39 @@ fun aresumof(test : Long, preamble : MutableList<Pair<Long,Long>>):Boolean = (tr
             acc
         }))
 
-fun part1() {
-    val m = getpreambles(numbers).fold(mutableListOf<Pair<MutableList<Pair<Long,Long>>,Long>>(),{
+// return the first number that does not match the rules
+fun part1():Long {
+    val m = getpreambles(numbers,26).fold(mutableListOf<Pair<MutableList<Pair<Long,Long>>,Long>>(),{
         acc, pair ->
         acc.add(Pair(preamblePairs(pair.first),pair.second))
         acc
     })
 
-    val theone = m.filter { !aresumof(it.second,it.first) }.toList()[0].second
-    println(theone)
+    return m.filter { !aresumof(it.second,it.first) }.toList()[0].second
 }
-/*
-fun findtheweakness(): List<List<Long>>{
-    val sumtoget : Long = 776203571
-    for(i in 1 until numbers.size)
-    {
-        val n = numbers.windowed(i).filter { it.sum() == sumtoget }
 
+//call window algorithm using different window size to find the numbers that add up to the sum to get
+fun part2(sumtoget:Long):Long{
+    fun getthesum(i : Int): MutableList<Pair<List<Long>,Long>> = getpreambles(numbers, i).filter { it.first.sum() == sumtoget }.toMutableList()
+
+    for(i in 1 until numbers.size) {
+        val n = getthesum(i)
+            if(n.size > 0 && n[0].first.size > 2) {
+            return (n.first().first.sortedDescending()[0] + n.first().first.sortedDescending()[n.first().first.size-1])
+        }
     }
+    return 0
 }
 
-fun part2()
-{
-println(findtheweakness())
-}
-*/
 fun main()
 {
-    part1()
-   // part2()
+    val p1 = part1()
+    val p2 = part2(p1)
+
+    println("Part 1 ")
+    println(p1)
+    println("Part 2")
+    println(p2)
 }
 
 main()
