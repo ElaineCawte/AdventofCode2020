8055ccefe6b291479861255aafc80159a0faf423
B1tchDuckD01an <elaine.g.dore@gmail.com>
2020-12-21 17:53:18 +0000

    Day10

--- Day10/day10.kts
+++ Day10/day10.kts
@@ -10,8 +10,23 @@ fun part1() : Int {
     return a.count { it == 1 } * (a.count { it == 3 } + 1)
 }
 
+fun part2() : Long {
+    val num = numbers.sorted()
+    val matches: MutableMap<Int,Long> = mutableMapOf(0 to 1L)
+
+    num.drop(1).forEach { i ->
+        matches[i] = (1 .. 3).map { a ->
+            matches.getOrDefault(i - a, 0)
+        }.sum()
+    }
+
+    return matches.getValue(num.last())
+}
+
 fun main() {
     println("part 1 ")
     println(part1())
+    println("part 2 ")
+    println(part2())
 }
 main()
